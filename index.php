1cf2f0adc82a3d91865a6bdbf094398e8e495448
kenn-64bit <160496199+kenn-64bit@users.noreply.github.com>
2025-05-15 21:03:11 +0800

    index backbone

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Record</title>
+
+    <?php
+    $conn = mysqli_connect("localhost", "root", "", "phpsample"); 
+    ?>
+</head>
+<body>
+    <center>
+        <a href="form.php">+Add Student</a>
+        <table border="1" width="80%" style="border-collapse: collapse;">
+        <tr>
+            <th> Student Number</th>
+        </tr>
+
+</body>
+</html>
