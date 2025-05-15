8498c310937e36f2df17bc049001446ed14d9fbb
Erckvnc <Erickvinceestabillo2@gmail.com>
2025-05-15 22:47:49 +0800

    asd

--- form_act.php
+++ form_act.php
@@ -8,10 +8,17 @@ $course=$_POST['course'];
 $email=$_POST['email'];
 $birthdate=$_POST['birthdate'];
 
-mysqli_query($conn,"INSERT INTO student VALUES ('$studentnum','$fName','$lName','$course','$email','$birthdate')");
+$result=mysqli_query($conn,"SELECT * FROM student WHERE studentnum='$studentnum'");
+if(mysqli_num_rows($result) > 0) {
+    echo "<script>
+    alert('Student Number already exists! Please enter a unique number.');
+    window.location='form.php';
+    </script>";
+} else {
+    mysqli_query($conn,"INSERT INTO student VALUES ('$studentnum','$fName','$lName','$course','$email','$birthdate')");
+    echo "<script>
+    alert('New Record Added!');
+    window.location='index.php';
+    </script>";
+}
 ?>
-
-<script>
-alert('New Record Added!');
-window.location="index.php";
-</script>
